c9a538892920d6612c0306f4493fa7f182648ede
Mathias <mathfernandes@usp.br>
2020-04-02 23:43:33 -0300

    js/challenge/highlight text

--- js/challenge_1/app.js
+++ js/challenge_1/app.js
@@ -1,5 +1,31 @@
-function putDecoration() {
-    console.log("putDecoration")
+function putDecoration(name, _debug = 0) {
+    if(_debug===0) {
+        console.log("putDecoration")
+    }
+    let text = document.getElementsByTagName(name)[0]
+    let words = text.innerHTML.split(" ")
+    let decorationText = ''
+    let aux;
+    for( let i = 0;i < words.length; ++i) {
+        if(_debug===0) {
+            decorationText += words[i].length
+        }
+        if(words[i].length >= 8) {
+            decorationText += '<marker class="destaque1">' + words[i] + '</marker> '
+        } else if(words[i].length === 6) {
+            decorationText += '<marker class="destaque2">' + words[i] + '</marker> '
+        } else if(words[i].length === 4 || words[i].length === 5) {
+            decorationText += '<marker class="destaque3">' + words[i] + '</marker> '
+        } else {
+            decorationText += '<marker>' + words[i] + '</marker> '
+        }
+    }
+
+    document.getElementsByTagName(name)[0].innerHTML = decorationText
+    if(_debug===0) {
+        console.log(words)
+    }
 }
 
-putDecoration()
+putDecoration("p", 1)
+putDecoration("h1", 1)
