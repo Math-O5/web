cd108026da1f493a813a7c103b8822dc37a575c5
Mathias <mathfernandes@usp.br>
2020-09-07 21:57:16 -0300

    Add city

--- js/automation/meadowfield.js
+++ js/automation/meadowfield.js
@@ -1,75 +1,43 @@
 // Proposed: https://eloquentjavascript.net/07_robot.html
-const roads = [
-    "Alice's House-Bob's House",   "Alice's House-Cabin",
-    "Alice's House-Post Office",   "Bob's House-Town Hall",
-    "Daria's House-Ernie's House", "Daria's House-Town Hall",
-    "Ernie's House-Grete's House", "Grete's House-Farm",
-    "Grete's House-Shop",          "Marketplace-Farm",
-    "Marketplace-Post Office",     "Marketplace-Shop",
-    "Marketplace-Town Hall",       "Shop-Town Hall"
-  ];
-
-// Proposed: https://eloquentjavascript.net/07_robot.html
-
-/**
- * @param  { string[] } edges indicates the connetions between places
- * @return { graph } graph is undirected reresented in an adjacent list.
- * 
- * This funtion receives a array of string with the format from - to, se above:
- * 
- * [ 
- *      "My house - Your house",
- *      "My heart - Your heart"
- * ]
- */
-function buildGraph(edges) {
-    
-    let graph = Object.create(null);
-    
-    function addEdge(from, to) {
-      if (graph[from] == null) {
-        graph[from] = [to];
-      } else {
-        graph[from].push(to);
-      }
-    }
-
-    for (let [from, to] of edges.map(r => r.split("-"))) {
-      addEdge(from, to);
-      addEdge(to, from);
-    }
-    return graph;
-  }
-  
-const roadGraph = buildGraph(roads);
 
 /**
  * 
  */
 class VillageState {
     constructor(place, parcels) {
-        this.place = place;
-        this.parcels = parcels;
+        this.place = place;     // current robot position
+        this.parcels = parcels; // all parcels that need to be delivered
     }
 
     move(destination) {
-        // if it is not possible go from this place to destination 
-        if (!roadGraph[this.place].includes(destination)) {
-            return this;
-        } else {
 
-            let parcels = this.parcels.map(p => {
-                if (p.place != this.place) return p;
-                    return {place: destination, address: p.address};
-        }).filter(p => p.place != p.address);
-            return new VillageState(destination, parcels);
-        }
+      // the state remains the same if there is no road to go.
+      if(!roadGraph[this.place].includes(destination)) {
+        return this;
+      } else {
+        // Change state of all moving parcel, 
+        this.parcels = this.parcels
+                           .map(parcel => {
+                              if(parcel.address != this.place) 
+                                return {place: destination, address: parcel.address};
+                              else return parcel;
+                           })
+                           .filter(parcel => parcel.address != this.place); 
+        console.log(this.parcels);
+        return new VillageState(destination, this.parcels);
+      }
     }
 }
 
-let first = new VillageState(
-    "Post Office",
-    [{place: "Post Office", address: "Alice's House"}]
-  );
-  let next = first.move("Alice's House");
-  
+VillageState.random = function(parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({place, address});
+  }
+  return new VillageState("Post Office", parcels);
+};
